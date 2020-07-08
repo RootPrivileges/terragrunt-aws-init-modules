92584e6d6671bdb7cc6b0bada64107cdcc35415a
Doug <rootprivileges@gmail.com>
2020-07-08 22:12:23 +0100

    Replace remote_state in terragrunt group module

--- iam/groups/terragrunt/variables.tf
+++ iam/groups/terragrunt/variables.tf
@@ -1,3 +1,28 @@
+variable "assume_terragrunt_administrator_role_policy_arn" {
+  description = "The ARN of the AssumeTerragruntAdministrator role policy"
+  type        = string
+}
+
+variable "management_org_account_access_role_policy_arn" {
+  description = "The ARN of the AssumeOrganizationAccountAccessRole policy in the Management account"
+  type        = string
+}
+
+variable "production_org_account_access_role_policy_arn" {
+  description = "The ARN of the AssumeOrganizationAccountAccessRole policy in the Production account"
+  type        = string
+}
+
+variable "staging_org_account_access_role_policy_arn" {
+  description = "The ARN of the AssumeOrganizationAccountAccessRole policy in the Staging account"
+  type        = string
+}
+
+variable "terragrunt_data_administrator_policy_arn" {
+  description = "The ARN of the TerragruntDataAdministrator policy, allowing write access to Terraform data"
+  type        = string
+}
+
 variable "tfstate_global_bucket" {
   description = "The S3 bucket that holds Terraform state"
   type        = string
