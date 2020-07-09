a0bdca69460d5382aa39e859f505957b76722f84
Doug <rootprivileges@gmail.com>
2020-07-09 19:59:40 +0100

    Remove remote_state from priv-subnet-without-nat

--- networking/private-subnet-without-nat/variables.tf
+++ networking/private-subnet-without-nat/variables.tf
@@ -1,3 +1,8 @@
+variable "account_id" {
+  description = "The ID of the account"
+  type        = string
+}
+
 variable "account_name" {
   description = "The name of the account"
   type        = string
@@ -25,5 +30,10 @@ variable "tfstate_global_bucket" {
 
 variable "tfstate_global_bucket_region" {
   description = "The region of the S3 bucket that holds Terraform state"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the account VPC"
   type        = string
 }
