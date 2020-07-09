a99a0bc274d72ef6a45038352d1b367ca1e19078
Doug <rootprivileges@gmail.com>
2020-07-09 19:53:00 +0100

    Remove remote_state from public-subnet module

--- networking/public-subnet/public-subnet.tf
+++ networking/public-subnet/public-subnet.tf
@@ -4,13 +4,13 @@ module "public_subnet" {
   availability_zone = "${var.availability_zone}"
   subnet_cidr       = var.subnet_cidr
   subnet_name       = "${var.availability_zone}-${var.subnet_name}"
-  vpc_id            = "${data.terraform_remote_state.environment_vpc.outputs.vpc_id}"
+  vpc_id            = "${var.vpc_id}"
 }
 
 resource "aws_route" "gateway_route" {
   route_table_id         = module.public_subnet.route_table_id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = data.terraform_remote_state.environment_vpc.outputs.internet_gateway_id
+  gateway_id             = var.vpc_internet_gateway_id
 }
 
 resource "aws_network_acl_rule" "public_acl_internet_in" {
