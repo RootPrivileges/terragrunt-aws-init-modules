7528ece2b94d46fa70f1337b2eb93a3c5ca22892
Doug <rootprivileges@gmail.com>
2020-07-11 16:31:53 +0100

    Add output descriptions to enable-guardduty module

--- utility/guardduty/enable-guardduty-in-one-region/outputs.tf
+++ utility/guardduty/enable-guardduty-in-one-region/outputs.tf
@@ -1,3 +1,4 @@
 output "detector_id" {
-  value = "${aws_guardduty_detector.default.id}"
+  value       = "${aws_guardduty_detector.default.id}"
+  description = "The ID of the GuardDuty detector"
 }
