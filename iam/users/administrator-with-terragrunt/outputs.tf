778549dc03d4d5de708faf3b1b29d1ee461f704a
Doug <rootprivileges@gmail.com>
2020-07-11 16:06:56 +0100

    Add descriptions to terragrunt-admin user module

--- iam/users/administrator-with-terragrunt/outputs.tf
+++ iam/users/administrator-with-terragrunt/outputs.tf
@@ -1,15 +1,19 @@
 output "admin_user_access_key" {
-  value = "${aws_iam_access_key.admin_user.id}"
+  value       = "${aws_iam_access_key.admin_user.id}"
+  description = "The access key for the administrator user"
 }
 
 output "admin_user_password" {
-  value = "${aws_iam_user_login_profile.admin_user.encrypted_password}"
+  value       = "${aws_iam_user_login_profile.admin_user.encrypted_password}"
+  description = "The login password for the administrator user"
 }
 
 output "admin_user_secret_key" {
-  value = "${aws_iam_access_key.admin_user.encrypted_secret}"
+  value       = "${aws_iam_access_key.admin_user.encrypted_secret}"
+  description = "The access secret for the administrator user"
 }
 
 output "admin_username" {
-  value = "${aws_iam_user.admin_user.name}"
+  value       = "${aws_iam_user.admin_user.name}"
+  description = "The login username for the administrator user"
 }
