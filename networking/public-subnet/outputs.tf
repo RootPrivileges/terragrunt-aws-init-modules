6258bb88953551690c89524fc5f21c64ccd1a7f4
Doug <rootprivileges@gmail.com>
2020-07-11 16:31:17 +0100

    Add output descriptions to public-subnet module

--- networking/public-subnet/outputs.tf
+++ networking/public-subnet/outputs.tf
@@ -1,15 +1,19 @@
 output "acl_id" {
-  value = "${module.public_subnet.acl_id}"
+  value       = "${module.public_subnet.acl_id}"
+  description = "The ID of the subnet network ACL"
 }
 
 output "cidr_block" {
-  value = "${var.subnet_cidr}"
+  value       = "${var.subnet_cidr}"
+  description = "The CIDR block of the subnet"
 }
 
 output "route_table_id" {
-  value = "${module.public_subnet.route_table_id}"
+  value       = "${module.public_subnet.route_table_id}"
+  description = "The ID of the subnet route table"
 }
 
 output "subnet_id" {
-  value = "${module.public_subnet.subnet_id}"
+  value       = "${module.public_subnet.subnet_id}"
+  description = "The ID of the subnet"
 }
