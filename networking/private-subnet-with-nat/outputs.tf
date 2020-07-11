d06170eb81c264900111f562430f11fee8d3d23b
Doug <rootprivileges@gmail.com>
2020-07-11 16:30:34 +0100

    Add descriptions to private-subnet-with-nat module

--- networking/private-subnet-with-nat/outputs.tf
+++ networking/private-subnet-with-nat/outputs.tf
@@ -1,19 +1,24 @@
 output "acl_id" {
-  value = "${module.private_subnet.acl_id}"
+  value       = "${module.private_subnet.acl_id}"
+  description = "The ID of the subnet network ACL"
 }
 
 output "cidr_block" {
-  value = "${var.subnet_cidr}"
+  value       = "${var.subnet_cidr}"
+  description = "The CIDR block of the subnet"
 }
 
 output "nat_gateway_id" {
   value = "${module.nat_gateway.nat_gateway_id}"
+  description = "The ID of the subnet NAT gateway"
 }
 
 output "route_table_id" {
-  value = "${module.private_subnet.route_table_id}"
+  value       = "${module.private_subnet.route_table_id}"
+  description = "The ID of the subnet route table"
 }
 
 output "subnet_id" {
-  value = "${module.private_subnet.subnet_id}"
+  value       = "${module.private_subnet.subnet_id}"
+  description = "The ID of the subnet"
 }
