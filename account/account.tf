90ab6fb07dd10307761a980f7f0f1e2f43bd9ac0
Doug <rootprivileges@gmail.com>
2020-08-16 14:29:23 +0100

    Move data block into data.tf

--- account/account.tf
+++ account/account.tf
@@ -1,18 +1,3 @@
-data "aws_iam_policy_document" "crossaccount_assume_from_master" {
-  statement {
-    sid     = "AssumeFromMaster"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "AWS"
-
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-      ]
-    }
-  }
-}
-
 resource "aws_organizations_account" "account" {
   name  = var.account_name
   email = "${var.account_email_slug}@${var.domain}"
