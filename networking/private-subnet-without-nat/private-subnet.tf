bff75d6f47445688fa142b34090586282c4094f6
Doug <rootprivileges@gmail.com>
2020-08-30 22:13:23 +0100

    Break formatting

--- networking/private-subnet-without-nat/private-subnet.tf
+++ networking/private-subnet-without-nat/private-subnet.tf
@@ -3,7 +3,7 @@ module "private_subnet" {
 
   availability_zone = var.availability_zone
   subnet_cidr       = var.subnet_cidr
-  subnet_name       = "${var.availability_zone}-${var.subnet_name}"
+  subnet_name        = "${var.availability_zone}-${var.subnet_name}"
   tags              = var.tags
   vpc_id            = var.vpc_id
 }
