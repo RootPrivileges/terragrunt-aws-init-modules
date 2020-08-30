38b6424fed97e65ad9fee0c35408f33fa56fe85e
Doug <rootprivileges@gmail.com>
2020-08-30 17:56:52 +0100

    Refactor out more data sources

--- utility/iam/create-vpc-flow-logs-publisher/main.tf
+++ utility/iam/create-vpc-flow-logs-publisher/main.tf
@@ -1,23 +1,3 @@
-data "aws_iam_policy_document" "vpc_flow_logs_publisher_assume_role_policy" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["vpc-flow-logs.amazonaws.com"]
-    }
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-data "aws_iam_policy_document" "vpc_flow_logs_publish_policy" {
-  statement {
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = ["*"]
-  }
-}
-
 resource "aws_iam_role" "vpc_flow_logs_publisher" {
   name               = "Assume${title(var.account_name)}${var.vpc_iam_role_name}"
   assume_role_policy = data.aws_iam_policy_document.vpc_flow_logs_publisher_assume_role_policy.json
