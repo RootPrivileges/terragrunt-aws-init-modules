00a5f30238ac4f47f00e273c75fe9e3271924804
Doug <rootprivileges@gmail.com>
2020-08-31 16:22:03 +0100

    Remove unneeded module

--- networking/private-subnet-without-nat/private-subnet.tf
+++
@@ -1,9 +0,0 @@
-module "private_subnet" {
-  source = "../../utility/networking/create-subnet"
-
-  availability_zone = var.availability_zone
-  subnet_cidr       = var.subnet_cidr
-  subnet_name       = "${var.availability_zone}-${var.subnet_name}"
-  tags              = var.tags
-  vpc_id            = var.vpc_id
-}
