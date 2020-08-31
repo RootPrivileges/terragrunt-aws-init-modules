698aa50fb5b67b698805aae1b05e2dd609a7063d
Doug <rootprivileges@gmail.com>
2020-08-31 12:43:51 +0100

    Fix comments and formatting

--- networking/vpc/public-subnets.tf
+++ networking/vpc/public-subnets.tf
@@ -1,20 +1,20 @@
-locals{
-    # Create a new list of subnets, one for each definied AZ of each defined public subnet
-   public_subnet_list =  flatten([
+locals {
+  # Create a new list of subnets, creating a new entry for each defined AZ of each defined public subnet
+  # (i.e. ["a", "b", "c"] in eu-west-1 will expand to three subnets, ["eu-west-1a", "eu-west-1b", "eu-west-1c", )
+  public_subnet_list = flatten([
     for subnet, config in var.public_subnets : [
       for az in config.availability_zones : {
         "${var.aws_region}${az}-${subnet}" = {
-          "cidr" = cidrsubnet(config.cidr, length(config.availability_zones)-1, index(config.availability_zones, az))
+          "cidr"              = cidrsubnet(config.cidr, length(config.availability_zones) - 1, index(config.availability_zones, az))
           "availability_zone" = "${var.aws_region}${az}"
-          "name" = subnet
+          "name"              = subnet
         }
       }
     ]
- ])
-
- public_subnets = { for item in local.public_subnet_list:
-     keys(item)[0] => values(item)[0]
-   }
+  ])
+  public_subnets = { for item in local.public_subnet_list :
+    keys(item)[0] => values(item)[0]
+  }
 }
 
 module "public_subnet" {
